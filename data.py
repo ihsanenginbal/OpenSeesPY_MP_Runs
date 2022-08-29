688fca7be156ad98da5531842428f3e744bf9f4e
İhsan Engin Bal <38361052+ihsanenginbal@users.noreply.github.com>
2022-08-29 15:36:44 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,82 @@
+#Frame102
+f_ro=[0.8, 1.0]  # no ref
+f_concrete=[0.75, 1.00, 1.51] # lower bound no ref, upper bound from our study IUSS Bal et al., 2008
+f_steel=[0.76, 1.00, 1.24]   # from Bal et al. 2008 IUSS CoV 0.24
+f_span=[1.00, 1.38]  # from Bal et al. 2008 IUSS CoV 0.38
+# This frame has some small spans, so we did not use here he lower bound
+f_ground_height=[1.00, 1.50]	# no ref, but ooking at some Bayrakli buildings
+f_upper_height=[0.87, 1.00, 1.13]  # from Bal et al. 2008 IUSS CoV 0.13 for PFN frames
+
+#Frame103
+f_ro=[0.8, 1.0]  # no ref
+f_concrete=[0.49, 1.00, 1.51] # lower bound and upper bound from our study IUSS Bal et al., 2008
+f_steel=[0.76, 1.00, 1.24]   # from Bal et al. 2008 IUSS CoV 0.24
+f_span=[1.00, 1.38]  # from Bal et al. 2008 IUSS CoV 0.38
+# This frame has some small spans, so we did not use here he lower bound
+f_ground_height=[1.00, 1.50]	# no ref, but ooking at some Bayrakli buildings
+f_upper_height=[0.87, 1.00, 1.13]  # from Bal et al. 2008 IUSS CoV 0.13 for PFN frames
+
+#Frame104
+f_ro=[0.8, 1.0]  # no ref
+f_concrete=[0.49, 1.00, 1.51] # lower bound and upper bound from our study IUSS Bal et al., 2008
+f_steel=[0.76, 1.00, 1.24]   # from Bal et al. 2008 IUSS CoV 0.24
+f_span=[1.00, 1.38]  # from Bal et al. 2008 IUSS CoV 0.38
+# This frame has some small spans, so we did not use here he lower bound
+f_ground_height=[1.00, 1.50]	# no ref, but ooking at some Bayrakli buildings
+f_upper_height=[0.87, 1.00, 1.13]  # from Bal et al. 2008 IUSS CoV 0.13 for PFN frames
+
+#PFN_2_1
+f_ro=[0.6, 0.8, 1.0]
+f_concrete=[0.50, 0.75, 1.00]
+f_steel=[0.75, 1.00, 1.50]
+f_span=[1.00, 1.50]
+f_ground_height=[0.75, 1.00]	
+f_upper_height=[0.75, 1.00]
+
+#PFN4_1
+f_ro=[0.8, 1.0]
+f_concrete=[0.75, 1.00, 1.50]
+f_steel=[0.75, 1.00, 1.50]
+f_span=[0.75, 1.00, 1.50]
+f_ground_height=[1.00, 1.50]	
+f_upper_height=[1.00]
+
+#PFN_4_2
+f_ro=[0.8, 1.0]
+f_concrete=[0.75, 1.00, 1.50]
+f_steel=[0.75, 1.00, 1.50]
+f_span=[1.00, 1.50]
+f_ground_height=[1.00, 1.50]	
+f_upper_height=[1.00,1.25]
+
+#PFN_5_1
+f_ro=[0.8, 1.0]
+f_concrete=[0.75, 1.00, 1.50]
+f_steel=[0.75, 1.00, 1.50]
+f_span=[1.00, 1.50]
+f_ground_height=[1.00, 1.50]	
+f_upper_height=[1.00,1.25]
+
+#PFN_5_2
+f_ro=[0.8, 1.0]
+f_concrete=[0.75, 1.00, 1.50]
+f_steel=[0.75, 1.00, 1.50]
+f_span=[1.00, 1.50]
+f_ground_height=[0.75, 1.00, 1.25]	
+f_upper_height=[1.00]
+
+#PFN_6_1
+f_ro=[0.8, 1.0]
+f_concrete=[0.75, 1.00, 1.50]
+f_steel=[0.75, 1.00, 1.50]
+f_span=[1.00, 1.50]
+f_ground_height=[0.75, 1.00, 1.25]	
+f_upper_height=[1.00]
+
+#PFN_7_1
+f_ro=[0.8, 1.0]
+f_concrete=[0.75, 1.00, 1.50]
+f_steel=[0.75, 1.00, 1.50]
+f_span=[1.00, 1.50]
+f_ground_height=[0.75, 1.00, 1.25]	
+f_upper_height=[1.00]
